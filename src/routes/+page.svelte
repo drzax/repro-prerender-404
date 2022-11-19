e1f2a7b80b2a3c2961bf7cf3a2c0462ccd8713a1
Simon Elvery <simon@elvery.net>
2022-11-19 17:58:39 +1000

    Minimise example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/blog/test/">A path that exists</a>
+<a href="/about/">A path that exists</a>
