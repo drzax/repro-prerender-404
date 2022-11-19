ab7c82d8d376d0d7d0dda88ff66e251268a282c7
Simon Elvery <simon@elvery.net>
2022-11-19 17:08:25 +1000

    Fix link from homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/test/">A path that exists</a>
+<a href="/blog/test/">A path that exists</a>
